671509407a48221948d0fbd95e56db72a82c6397
Nygil Pereira <nygilgp@gmail.com>
2021-07-06 16:39:30 +0530

    Close auctions after they expire

--- src/handlers/placeBid.js
+++ src/handlers/placeBid.js
@@ -11,13 +11,17 @@ async function placeBid(event, context) {
 
 	const auction = await getAuctionById(id);
 
+	if (auction.status !== 'OPEN') {
+		throw new createError.Forbidden(`Your cannot bid on closed auctions!`);
+	}
+
 	if (amount <= auction.highestBid.amount) {
 		throw new createError.Forbidden(`Your bid must be higher than ${auction.highestBid.amount}!`);
 	}
 
 	const params = {
 		TableName: process.env.AUCTIONS_TABLE_NAME,
-    Key: { id },
+    	Key: { id },
 		UpdateExpression: 'set highestBid.amount = :amount',
 		ExpressionAttributeValues: {
 			':amount': amount,
