0366762e65903e88e24f7b4bc896f35b3f9a2900
Nygil Pereira <nygilgp@gmail.com>
2021-07-08 00:53:38 +0530

    Added authorization on sls services and used data of user

--- README.md
+++ README.md
@@ -15,3 +15,14 @@ npm install
 ```
 
 You are ready to go!
+
+
+curl --location --request POST 'https://YOUR_AUTH0_DOMAIN/oauth/token' \
+--header 'Content-Type: application/x-www-form-urlencoded' \
+--data-urlencode 'client_id=YOUR_AUTH0_CLIENT_ID' \
+--data-urlencode 'username=YOUR_USERNAME' \
+--data-urlencode 'password=YOUR_PASSWORD' \
+--data-urlencode 'grant_type=password' \
+--data-urlencode 'scope=openid'
+
+https://jwt.io/
--- src/handlers/placeBid.js
+++ src/handlers/placeBid.js
@@ -10,13 +10,23 @@ const dynamodb = new AWS.DynamoDB.DocumentClient();
 async function placeBid(event, context) {
 	const { id } = event.pathParameters;
 	const { amount } = event.body;
-
+	const { email } = event.requestContext.authorizer;
 	const auction = await getAuctionById(id);
+	// Bid identity validation
+	if (auction.seller === email) {
+		throw new createError.Forbidden(`You cannot bid on your own auctions!`)
+	}
+	// Avoid dounle bidding 
+	if (auction.highestBid.bidder === email) {
+		throw new createError.Forbidden(`You are already the highest bidder!`)
+	}
 
+	// Auction status validation
 	if (auction.status !== 'OPEN') {
 		throw new createError.Forbidden(`Your cannot bid on closed auctions!`);
 	}
 
+	// Auction bid amount valdiation
 	if (amount <= auction.highestBid.amount) {
 		throw new createError.Forbidden(`Your bid must be higher than ${auction.highestBid.amount}!`);
 	}
@@ -24,9 +34,10 @@ async function placeBid(event, context) {
 	const params = {
 		TableName: process.env.AUCTIONS_TABLE_NAME,
     	Key: { id },
-		UpdateExpression: 'set highestBid.amount = :amount',
+		UpdateExpression: 'set highestBid.amount = :amount, highestBid.bidder = :bidder',
 		ExpressionAttributeValues: {
 			':amount': amount,
+			':bidder': email,
 		},
 		ReturnValues: 'ALL_NEW',
 	}
